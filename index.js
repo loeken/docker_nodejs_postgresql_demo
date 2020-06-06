2b0a984a948c06c0b92c2df4f9a8bab2b33af3c9
loeken <loeken@internetz.me>
2020-06-06 13:40:48 +0200

    code example

---
+++ index.js
@@ -0,0 +1,18 @@
+const pg = require('pg');
+
+const cs = 'postgres://postgres:topsecure@172.18.0.2:5432/api';
+
+const client = new pg.Client(cs);
+client.connect();
+
+client.query('SELECT * FROM users').then(res => {
+
+    const data = res.rows;
+
+    console.log('all data');
+    data.forEach(row => {
+        console.log(`Id: ${row.id} Name: ${row.name} Email: ${row.email}`);
+    })
+}).finally(() => {
+    client.end()
+});
